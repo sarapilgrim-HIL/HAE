698ac703ae7532b30809ec914f78c329fdece89e
jake.grim.contractor <jake.grim.contractor@appian.com>
2016-08-03 14:49:13 -0400

    HAE - 28

---
+++ etc/sql/inserts/HAE 4.0.0.0/mtn_reference_postTypeInsert.sql.sql
@@ -0,0 +1,7 @@
+insert into HAE_REFERENCE (LABEL, VALUE, CATEGORY, DELETED) 
+values ('Announcement', '#Announcement', 'Post Type',0),
+('Question','#Question','Post Type',0),
+('Best Practice/ Lessons Learned','#Best Practice/ Lessons Learned','Post Type',0),
+('Business Question','#Business Question','Post Type',0),
+('Technical Question','#Technical Question','Post Type',0),
+('Request for Information','#Request for Information','Post Type',0);
