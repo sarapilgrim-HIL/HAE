8071c6b869cdd9c2940cc7d1836798610b300065
PERiddick0 <patrick.riddick@hil.us>
2016-08-16 14:39:29 -0400

    HAE Reference table Application insert
    HAE Reference table Application insert

--- etc/sql/inserts/HAE 5.0.0.0/HAE_reference_applicationInsert.sql
+++ etc/sql/inserts/HAE 5.0.0.0/HAE_reference_applicationInsert.sql
@@ -1,4 +1,4 @@
 insert into HAE_REFERENCE (LABEL, VALUE, CATEGORY, DELETED) 
-values ('Multi-Team Network', 'Multi-Team Network', 'Application',0),
-('Expense Reporting System','Expense Reporting System','Application',0),
-('Electronic Contract Filing System','Application','Application', 0);
+values ('Multi-Team Network', 'MTN', 'Application',0),
+('Expense Reporting System','ERS','Application',0),
+('Electronic Contract Filing System','ECFS','Application', 0);
