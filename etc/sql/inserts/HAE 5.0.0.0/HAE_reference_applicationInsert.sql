9942a8d9cb5d2629ff2cbd1fddb2e41dd81a1b8e
PERiddick0 <patrick.riddick@hil.us>
2016-08-16 14:01:55 -0400

    HAE Application insert into HAE_Reference
    Insert application names into HAE_Reference table

---
+++ etc/sql/inserts/HAE 5.0.0.0/HAE_reference_applicationInsert.sql
@@ -0,0 +1,4 @@
+insert into HAE_REFERENCE (LABEL, VALUE, CATEGORY, DELETED) 
+values ('Multi-Team Network', 'Multi-Team Network', 'Application',0),
+('Expense Reporting System','Expense Reporting System','Application',0),
+('Electronic Contract Filing System','Application','Application', 0);
