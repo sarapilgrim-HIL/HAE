8ed1f33f80ab369bbca8a77e489c88acf9000575
Brett Rojas <brett.rojas@ahil.us>
2017-02-15 09:42:56 -0500

    fixed typo

--- etc/httpd/conf/internalCopyHttpd.sh
+++ etc/httpd/conf/internalCopyHttpd.sh
@@ -1,5 +1,5 @@
 rm -rf /apps/serverApps/apache/conf/httpd.conf.OLD;
 mv /apps/serverApps/apache/conf/httpd.conf /apps/serverApps/apache/conf/httpd.conf.OLD;
 cp /home/apache/HAE/etc/httpd/conf/httpd.conf.internal /apps/serverApps/apache/conf/httpd.conf;
-/apps/serverApps/apache/bin/apache/apachectl stop;
-/apps/serverApps/apache/bin/apache/apachectl start;
+/apps/serverApps/apache/bin/apachectl stop;
+/apps/serverApps/apache/bin/apachectl start;
