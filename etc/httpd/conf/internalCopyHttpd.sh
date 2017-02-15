a8dab5ecb64d7565bf9add4471c407931e511055
Brett Rojas <brett.rojas@ahil.us>
2017-02-14 19:02:42 -0500

    Created script to copy httpd for internal environment

---
+++ etc/httpd/conf/internalCopyHttpd.sh
@@ -0,0 +1,3 @@
+rm -rf /apps/serverApps/apache/conf/httpd.conf.OLD;
+mv /apps/serverApps/apache/conf/httpd.conf /apps/serverApps/apache/conf/httpd.conf.OLD;
+cp /home/apache/HAE/etc/httpd/conf/httpd.conf.internal /apps/serverApps/apache/conf/httpd.conf;
