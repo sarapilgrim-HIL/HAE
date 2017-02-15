29083a1c434a4ce59ad967e240ff84f172a7534b
Brett Rojas <brett.rojas@ahil.us>
2017-02-15 09:50:01 -0500

    made executable

--- etc/httpd/conf/internalCopyHttpd.sh
+++ etc/httpd/conf/internalCopyHttpd.sh
@@ -2,4 +2,4 @@ rm -rf /apps/serverApps/apache/conf/httpd.conf.OLD;
 mv /apps/serverApps/apache/conf/httpd.conf /apps/serverApps/apache/conf/httpd.conf.OLD;
 cp /home/apache/HAE/etc/httpd/conf/httpd.conf.internal /apps/serverApps/apache/conf/httpd.conf;
 /apps/serverApps/apache/bin/apachectl stop;
-/apps/serverApps/apache/bin/apachectl start;
+/apps/serverApps/apache/bin/apachectl start; 
