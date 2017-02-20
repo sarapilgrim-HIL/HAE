e7b43e2afb1393a1df1e421c956e0e80f475f149
Brett Rojas <brett.rojas@ahil.us>
2017-02-19 23:09:03 -0500

    Updated db table names

--- AppianWorld17/Windows Code/HIL Demo/demo.py
+++ AppianWorld17/Windows Code/HIL Demo/demo.py
@@ -13,19 +13,19 @@ import datetime
 db = MySQLdb.connect(host="localhost",    # your host, usually localhost
                      user="root",         # your username
                      passwd="Password1!",  # your password
-                     db="aw_test")        # name of the data base
+                     db="appian_world")        # name of the data base
 
 cur = db.cursor()
 
 global newGameNumber
-ser = serial.Serial('COM3', 9600)
+ser = serial.Serial('COM4', 9600)
 sleepTime = .1
 
 def newGame():
     timestamp = datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')
-    cur.execute("INSERT INTO GAME (START_TIME) values (%s)", (timestamp, ))
+    cur.execute("INSERT INTO NTS_GAME (START_TIME) values (%s)", (timestamp, ))
     db.commit()
-    newGameNumber = cur.execute("SELECT MAX(ID) FROM GAME GROUP BY ID") + 0
+    newGameNumber = cur.execute("SELECT MAX(ID) FROM NTS_GAME GROUP BY ID") + 0
     print('New Game: ' + str(newGameNumber) + '\n')
     return newGameNumber
 
@@ -41,7 +41,7 @@ while True:
             newGameNumber = newGame()
         else: 
             print('Target Hit! ' + str(data.strip()) + ' points!' + '\n')
-            cur.execute('INSERT INTO SHOT (GAME_ID, POINTS) values (%s, %s)', (newGameNumber, data))
+            cur.execute('INSERT INTO NTS_SHOT (NTS_GAME_ID, POINTS) values (%s, %s)', (newGameNumber, data))
             db.commit()
 
     except:
